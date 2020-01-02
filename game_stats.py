f3ce213748ddaa088591f341ddbc0b1e1769d758
Gugoole <zeroxus@outlook.com>
2020-01-02 20:11:08 +0800

    add aliens sprites and quit games

--- game_stats.py
+++ game_stats.py
@@ -1,18 +1,24 @@
+import game_functions as gf
+
 class GameStats():
     """
     跟踪游戏统计信息
     """
-    def __init__(self,ai_settings):
+
+    def __init__(self, ai_settings):
         """
         初始化统计信息
         :param ai_settings:
         """
-        self.game_active=True
-        self.ai_settings=ai_settings
+        self.game_active = False
+        self.ai_settings = ai_settings
+
         self.reset_stats()
+
     def reset_stats(self):
         """
         初始化在运行期间可能变化的统计信息
         :return:
         """
-        self.ships_left=self.ai_settings.ship_limit
+        self.ships_left = self.ai_settings.ship_limit
+
